d5a4e8b4507c363541196f4b2d930a0fe9719d37
Sam Brez <sambreslin02@gmail.com>
2024-03-18 17:27:11 -0400

    undo test

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -39,20 +39,21 @@ router.get("/:id", async (req, res) => {
 });
 
 // Create a new comment
-router.post("/", withAuth, (req, res) => {
-  Comment.create({
-      comment: req.body.comment,
-      post_id: req.body.post_id,
-      user_id: req.session.user_id
-  }, (err, newComment) => {
-      if (err) {
-          res.status(500).json(err);
-      } else if (newComment) {
-          res.status(200).json(newComment);
-      } else {
-          res.status(400).json({ message: "No comment submitted" });
-      }
-  });
+router.post("/", withAuth, async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+        comment: req.body.comment,
+        post_id: req.body.post_id,
+        user_id: req.session.user_id
+        });
+
+        if (newComment) {res.status(200).json(newComment);
+        } else {
+            res.status(400).json({message: "No comment submitted"});
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 module.exports = router;
